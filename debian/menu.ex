d1c9e7cde449ec7be95aaae7ba060093122b8d57
Devon Kearns <dookie@kali.org>
2013-01-07 18:26:28 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ohrwurm):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ohrwurm" command="/usr/bin/ohrwurm"
--- debian/ohrwurm.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ohrwurm package
-#
-0 4	* * *	root	[ -x /usr/bin/ohrwurm_maintenance ] && /usr/bin/ohrwurm_maintenance
